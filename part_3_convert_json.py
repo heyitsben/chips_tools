b337e05c9efe81c9663475466f7922b94b748195
heyitsben <bennerr@andrew.cmu.edu>
2024-02-19 23:20:54 -0500

    edits

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -8,5 +8,10 @@ input_json_file = "data/bennerr_cc1.json"
 with open(input_json_file, "r") as reader:
     json_input = json.load(reader)
 #Convert JSON data to CCLevelPack
-
+levels = json_input["levels"]
+for level in levels:
+    title = cc_classes.CCMapTitleField(level["title"])
+    password = cc_classes.CCEncodedPasswordField(level["password"])
+    hint = cc_classes.CCMapHintField(level["hint"])
+    monster = cc_classes.CCMonsterMovementField(level["monster"])
 #Save converted data to DAT file
