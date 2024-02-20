4a92475f4ea019d76f08318d31aef938f3b0855f
heyitsben <bennerr@andrew.cmu.edu>
2024-02-19 22:25:30 -0500

    edits

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -4,5 +4,9 @@ import cc_classes
 
 #Part 3
 #Load your custom JSON file
+input_json_file = "data/bennerr_cc1.json"
+with open(input_json_file, "r") as reader:
+    json_input = json.load(reader)
 #Convert JSON data to CCLevelPack
+
 #Save converted data to DAT file
