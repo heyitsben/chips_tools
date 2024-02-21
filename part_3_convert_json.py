5526b14898296c55162b7ea63dcd3709d023d688
heyitsben <bennerr@andrew.cmu.edu>
2024-02-20 19:06:26 -0500

    edits

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -14,21 +14,35 @@ for level in levels:
     title = cc_classes.CCMapTitleField(level["title"])
     password = cc_classes.CCEncodedPasswordField(level["password"])
     hint = cc_classes.CCMapHintField(level["hint"])
-    monster = cc_classes.CCMonsterMovementField(level["monster"])
+
+    monsterTemp = level["monster"]
+    monster_coords = []
+    for m in monsterTemp:
+        mc = cc_classes.CCCoordinate(monsterTemp[0],monsterTemp[1])
+        monster_coords.append(mc)
+    monster = cc_classes.CCMonsterMovementField(monster_coords)
+
     level_number = level["level_number"]
     time = level["time"]
     chip_count = level["chip_count"]
     upper_layer = level["top_layer"]
 
     new_level = cc_classes.CCLevel()
+
     new_level.level_number = level_number
     new_level.time = time
     new_level.num_chips = chip_count
     new_level.upper_layer = upper_layer
+
+    new_level.add_field(title)
+    new_level.add_field(password)
+    new_level.add_field(hint)
+    new_level.add_field(monster)
+
     new_level.title = title
     new_level.password = password
     new_level.hint = hint
-    new_level.monsters = monster
+    new_level.monsters = monster_coords
 
     level_pack.add_level(new_level)
 #Save converted data to DAT file
